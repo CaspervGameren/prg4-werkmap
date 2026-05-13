2ce0340a789cd850941a99747318914c069c1733
Casper <caspervgameren@outlook.com>
2026-05-13 11:12:09 +0200

    ADDED wack-a-mole a part of les-2

---
+++ wack-a-mole/src/ts/game.ts
@@ -0,0 +1,62 @@
+import '../css/style.css';
+import { Actor, Engine, Font, Label, Vector } from "excalibur";
+import { ResourceLoader, Resources } from './resources.ts';
+
+export class Game extends Engine {
+
+    score = 0;
+    scoreLabel: any;
+
+    constructor() {
+        super({
+            width: 1024,
+            height: 720,
+        });
+        this.start(ResourceLoader).then(() => this.startGame());
+    }
+
+    onInitialize(engine: Engine) {
+        console.log("initializing game");
+
+        this.scoreLabel = new Label({
+            text: 'Score: 0',
+            pos: new Vector(this.drawWidth / 2, 25),
+            font: new Font({ size: 30 }),
+        });
+        this.add(this.scoreLabel);
+    }
+
+    startGame() {
+        console.log("start de game!");
+
+        for (let i = 0; i < 10; i++) {
+            this.spawnPile();
+        }
+    }
+
+    spawnPile() {
+        const pile = new Actor();
+        this.add(pile);
+        const isMole = Math.random() > 0.5;
+
+        if (isMole) {
+            pile.graphics.use(Resources.Mole.toSprite());
+        } else {
+            pile.graphics.use(Resources.DirtPile.toSprite());
+        }
+        pile.pos.x = Math.random() * this.drawWidth;
+
+        pile.pos.y = Math.random() * this.drawHeight;
+
+        pile.on('pointerdown', () => {
+            if (isMole) {
+                this.score++;
+            } else {
+                this.score--;
+            }
+            this.scoreLabel.text = `Score: ${this.score}`;
+        });
+    }
+}
+
+new Game();
